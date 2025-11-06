9bb5f6b641d890051791d55cca550abdef29512a
Elbound <theostanza@gmail.com>
2025-11-06 09:34:49 +0700

    05-11-25 end sesi 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import React from "react"
+import { createRoot } from "react-dom/client"
+import Pet from "./Pet"
+
+
+const App = () => {
+    return(
+    <div>
+      <h1>Pet Rescue App</h1>
+      <Pet name="Ica" animal="Pig" breed="Boar" />
+      <Pet name="Bob" animal="Dog" breed="Border Collie" />
+      <Pet name="Carla" animal="cat" breed="Jalanan" />
+    </div>
+  )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
